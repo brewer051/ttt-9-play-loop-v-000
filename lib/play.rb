8a37b8ee05d6c858852d835da46e4b32f8754652
Justin B <brewer051@gmail.com>
2018-06-19 01:03:21 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,15 @@ def turn(board)
 end
 
 # Define your play method below
+
+describe '#play' do
+  it 'calls turn nine times' do
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+    allow(self).to receive(:gets).and_return('1','2','3','4','5','6','7','8','9')
+    
+    play(board)
+
+    expect(board).to eq(["X","X","X","X","X","X","X","X","X",])
+  end
+end
